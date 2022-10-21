59c2e957c4e2cb208ec8fe9ff3a68314c21817e8
Marc Prud'hommeaux <mwp1@cornell.edu>
2022-10-20 20:44:36 -0400

    Update fairtool to 0.6.52

--- Formula/fairtool.rb
+++ Formula/fairtool.rb
@@ -1,16 +1,8 @@
 class Fairtool < Formula
   desc "Tools for managing an ecosystem of app sources"
   homepage "https://github.com/fair-ground/Fair"
-  url "https://github.com/fair-ground/Fair.git", tag: "0.6.51", revision: "d078218f8185f613b07a0373cadfa4811a421319"
+  url "https://github.com/fair-ground/Fair.git", tag: "0.6.52", revision: "d6e9606c84e92189a6b51b4514cb3023bdb4292a"
   license "AGPL-3.0"
-
-  bottle do
-    root_url "https://github.com/fair-ground/Fair/releases/download/0.6.51"
-
-    sha256 cellar: :any, arm64_monterey: "680de0f9fcacdbfb56aee83235779f559fe5fd322a0f7b10c24f4ba841bd2f67"
-    sha256 cellar: :any, monterey: "650b08a8064dd97b20d2552a1b8ce4b26c3c9a6de0899ee9e5c0e5d54a13f3a9"
-    sha256 cellar: :any, x86_64_linux: "b678bc27bb968c726e1ec64adeb6d62448052dc2c2c9b109b958fe43f2806f5d"
-  end
 
   head "https://github.com/fair-ground/Fair.git", branch: "main"
 
