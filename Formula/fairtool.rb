4929de7e53e7838e228bd7157128491149c93df6
Marc Prud'hommeaux <mwp1@cornell.edu>
2022-11-05 14:20:57 -0400

    Add bottles for fairtool 0.7.9

--- Formula/fairtool.rb
+++ Formula/fairtool.rb
@@ -4,6 +4,14 @@ class Fairtool < Formula
   url "https://github.com/fair-ground/Fair.git", tag: "0.7.9", revision: "39fd28a68b57aa073fd8da05aa02b0a2de75d635"
   license "AGPL-3.0"
 
+  bottle do
+    root_url "https://github.com/fair-ground/Fair/releases/download/0.7.9"
+
+    sha256 cellar: :any, arm64_monterey: "053825bf004310df6e8a7c5d3c59bb7278368b2f4e26ca329c7d92c0891ed03e"
+    sha256 cellar: :any, monterey: "cef7acb3bdcd3d6e7002bdf69e04bef8d5da4bba3f7e4b85c737c4edfe169ca9"
+    sha256 cellar: :any, x86_64_linux: "cddfc7d43b2266ccfdd29a1b730fb9fa7506443b1cbf564100a9ed1e557b5baf"
+  end
+
   head "https://github.com/fair-ground/Fair.git", branch: "main"
 
   uses_from_macos "swift"
