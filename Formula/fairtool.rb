8be5f54834ded7fb4b7e2553b5e5f6f4ad1a90f6
Marc Prud'hommeaux <marc@appfair.org>
2022-11-21 23:45:11 -0500

    Update fairtool to 0.7.47

--- Formula/fairtool.rb
+++ Formula/fairtool.rb
@@ -1,7 +1,7 @@
 class Fairtool < Formula
   desc "Tools for managing an ecosystem of app sources"
   homepage "https://github.com/fair-ground/Fair"
-  url "https://github.com/fair-ground/Fair.git", tag: "0.7.46", revision: "620306f3e584835c655504f4f00bcb64a2a3d582"
+  url "https://github.com/fair-ground/Fair.git", tag: "0.7.47", revision: "bab7893d683fd5800431def75096e40a677ead40"
   license "AGPL-3.0"
 
   head "https://github.com/fair-ground/Fair.git", branch: "main"
